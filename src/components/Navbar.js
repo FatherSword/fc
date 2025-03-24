f96dc0cbcb877332d4e1ebe79da32d5851f45671
Sword <endless_sword@163.com>
2025-03-24 20:30:55 +0800

    1-

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
-const Navbar = () => {
+const BottomNavbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <ul className="flex space-x-4">
-        <li><Link href="/" className="text-white">主页</Link></li>
-        <li><Link href="/players" className="text-white">陪玩打手</Link></li>
-        <li><Link href="/announcements" className="text-white">公告</Link></li>
-        <li><Link href="/profile" className="text-white">个人中心</Link></li>
-      </ul>
-    </nav>
+    <div className="fixed bottom-4 w-full bg-gray-800 p-4 flex flex-row justify-between items-center">
+          <Link href="/" className="text-white flex flex-col items-center space-y-1">
+            <Image src="/home.svg" alt="主页" width={24} height={24} />
+            <span className="text-sm">主页</span>
+          </Link>
+
+          <Link href="/players" className="text-white flex flex-col items-center space-y-1">
+            <Image src="/peiwan.svg" alt="陪玩打手" width={24} height={24} />
+            <span className="text-sm">陪玩打手</span>
+          </Link>
+        
+          <Link href="/announcements" className="text-white flex flex-col items-center space-y-1">
+            <Image src="/gonggao.svg" alt="公告" width={24} height={24} />
+            <span className="text-sm">公告</span>
+          </Link>
+       
+          <Link href="/profile" className="text-white flex flex-col items-center space-y-1">
+            <Image src="/my.svg" alt="个人中心" width={24} height={24} />
+            <span className="text-sm">个人中心</span>
+          </Link>
+    </div>
   )
 }
 
-export default Navbar    
+export default BottomNavbar
